420222c43884baf2329852e3bf9f1d8020d8c065
Frank <mueller@ubilabs.net>
2010-07-28 18:20:13 +0200

    update remine_github_hook Plugin

--- vendor/plugins/redmine_github_hook/app/controllers/github_hook_controller.rb
+++ vendor/plugins/redmine_github_hook/app/controllers/github_hook_controller.rb
@@ -1,28 +1,66 @@
 require 'json'
+require 'open3'
 
 class GithubHookController < ApplicationController
-  
+
   skip_before_filter :verify_authenticity_token, :check_if_login_required
 
   def index
+    repository = find_repository
 
-    Repository.all.each do |repository|
-      if repository.is_a?(Repository::Git)
-        puts repository.url
-        command = "cd '#{repository.url}/../' && git pull"
-        exec(command)
-      end
-    end
-    Repository.fetch_changesets
+    # Fetch the changes from Github
+    update_repository(repository)
+
+    # Fetch the new changesets into Redmine
+    repository.fetch_changesets
+
     render(:text => 'OK')
   end
 
   private
-  
+
   def exec(command)
-    logger.info { "GithubHook: Executing command: '#{command}'" }
-    output = `#{command}`
-    logger.info { "GithubHook: Shell returned '#{output}'" }
+    logger.debug { "GithubHook: Executing command: '#{command}'" }
+    stdin, stdout, stderr = Open3.popen3(command)
+
+    output = stdout.readlines.collect(&:strip)
+    errors = stderr.readlines.collect(&:strip)
+
+    logger.debug { "GithubHook: Output from git:" }
+    logger.debug { "GithubHook:  * STDOUT: #{output}"}
+    logger.debug { "GithubHook:  * STDERR: #{output}"}
+  end
+
+  # Fetches updates from the remote repository
+  def update_repository(repository)
+    command = "cd '#{repository.url}' && git fetch origin && git reset --soft refs/remotes/origin/master"
+    exec(command)
+  end
+
+  # Gets the project identifier from the querystring parameters and if that's not supplied, assume
+  # the Github repository name is the same as the project identifier.
+  def get_identifier
+    payload = JSON.parse(params[:payload])
+    identifier = params[:project_id] || payload['repository']['name']
+    raise ActiveRecord::RecordNotFound, "Project identifier not specified" if identifier.nil?
+    return identifier
+  end
+
+  # Finds the Redmine project in the database based on the given project identifier
+  def find_project
+    identifier = get_identifier
+    project = Project.find_by_identifier(identifier.downcase)
+    raise ActiveRecord::RecordNotFound, "No project found with identifier '#{identifier}'" if project.nil?
+    return project
+  end
+
+  # Returns the Redmine Repository object we are trying to update
+  def find_repository
+    project = find_project
+    repository = project.repository
+    raise TypeError, "Project '#{project.to_s}' ('#{project.identifier}') has no repository" if repository.nil?
+    raise TypeError, "Repository for project '#{project.to_s}' ('#{project.identifier}') is not a Git repository" unless repository.is_a?(Repository::Git)
+    return repository
   end
 
 end
--- vendor/plugins/redmine_github_hook/test/functional/github_hook_controller_test.rb
+++ vendor/plugins/redmine_github_hook/test/functional/github_hook_controller_test.rb
@@ -52,9 +52,17 @@ class GithubHookControllerTest < ActionController::TestCase
     @project = Project.new
     @project.stubs(:repository).returns(@repository)
     Project.stubs(:find_by_identifier).with('github').returns(@project)
-    @controller.stubs(:exec)
+
+    # Make sure we don't run actual commands in test
+    Open3.stubs(:popen3)
 
     Repository.expects(:fetch_changesets).never
+  end
+
+  def mock_descriptor(kind, contents = [])
+    descriptor = mock(kind)
+    descriptor.expects(:readlines).returns(contents)
+    descriptor
   end
 
   def do_post(payload = nil)
@@ -65,6 +73,7 @@ class GithubHookControllerTest < ActionController::TestCase
 
   def test_should_use_the_repository_name_as_project_identifier
     Project.expects(:find_by_identifier).with('github').returns(@project)
+    @controller.stubs(:exec).returns(true)
     do_post
   end
 
@@ -74,17 +83,38 @@ class GithubHookControllerTest < ActionController::TestCase
     do_post
   end
 
+  def test_should_use_project_identifier_from_request
+    Project.expects(:find_by_identifier).with('redmine').returns(@project)
+    @controller.stubs(:exec).returns(true)
+    post :index, :project_id => 'redmine', :payload => @json
+  end
+
+  def test_should_downcase_identifier
+    # Redmine project identifiers are always downcase
+    Project.expects(:find_by_identifier).with('redmine').returns(@project)
+    @controller.stubs(:exec).returns(true)
+    post :index, :project_id => 'ReDmInE', :payload => @json
+  end
+
   def test_should_render_ok_when_done
+    @controller.expects(:exec).returns(true)
     do_post
     assert_response :success
     assert_equal 'OK', @response.body
   end
 
   def test_should_fetch_changesets_into_the_repository
+    @controller.expects(:exec).returns(true)
     @repository.expects(:fetch_changesets).returns(true)
     do_post
     assert_response :success
     assert_equal 'OK', @response.body
+  end
+
+  def test_should_return_404_if_project_identifier_not_given
+    assert_raises ActiveRecord::RecordNotFound do
+      do_post :repository => {}
+    end
   end
 
   def test_should_return_404_if_project_not_found
@@ -96,7 +126,7 @@ class GithubHookControllerTest < ActionController::TestCase
 
   def test_should_return_500_if_project_has_no_repository
     assert_raises TypeError do
-      project = mock('project')
+      project = mock('project', :to_s => 'My Project', :identifier => 'github')
       project.expects(:repository).returns(nil)
       Project.expects(:find_by_identifier).with('github').returns(project)
       do_post :repository => {:name => 'github'}
@@ -105,7 +135,7 @@ class GithubHookControllerTest < ActionController::TestCase
 
   def test_should_return_500_if_repository_is_not_git
     assert_raises TypeError do
-      project = mock('project')
+      project = mock('project', :to_s => 'My Project', :identifier => 'github')
       repository = Repository::Subversion.new
       project.expects(:repository).at_least(1).returns(repository)
       Project.expects(:find_by_identifier).with('github').returns(project)
@@ -114,7 +144,17 @@ class GithubHookControllerTest < ActionController::TestCase
   end
 
   def test_should_not_require_login
+    @controller.expects(:exec).returns(true)
     @controller.expects(:check_if_login_required).never
+    do_post
+  end
+
+  def test_exec_should_log_output_from_git_as_debug
+    stdout = mock_descriptor('STDOUT', ["output 1\n", "output 2\n"])
+    stderr = mock_descriptor('STDERR', ["error 1\n", "error 2\n"])
+    Open3.expects(:popen3).returns(['STDIN', stdout, stderr])
+
+    @controller.logger.expects(:debug).at_least(4)
     do_post
   end
 
